157d1a6b15660ae81b9b523371f3250e753702ea
Sonu Kumar <cehsonu100@gmail.com>
2021-07-30 11:00:03 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+import Demo from './Demo';
+import DragDrop from './DragDrop';
+import DragDemo from './DragDemo';
+import SimpleList from './SimpleList'
+import DragDropHtmlTag from './DragDropHtmlTag';
+
+function App() {
+
+  return (
+    <div className="App">
+        <DragDropHtmlTag/>
+    </div>
+  );
+}
+
+export default App;
